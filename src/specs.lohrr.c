90c5fc9b9824d73f3eb1b3215d9b91148cb96ae9
Lohrr <Lohrr2k@yahoo.com>
2011-09-27 01:23:11 +0000

    The Leviathan procs are in!  Might need beautification.  Makefiles now put dms_new in the mud directory.

--- src/specs.lohrr.c
+++ src/specs.lohrr.c
@@ -3,9 +3,11 @@
 #include "interp.h"
 #include "comm.h"
 #include "prototypes.h"
+#include "ships.h"
 extern P_room world;
 
 int adjacent_room_nesw(P_char ch, int num_rooms );
+P_ship leviathan_find_ship( P_char leviathan, int room, int num_rooms );
 
 // This is an old proc for Lohrr's eq..
 void proc_lohrr( P_obj obj, P_char ch, int cmd, char *argument )
@@ -38,75 +40,6 @@ void proc_lohrr( P_obj obj, P_char ch, int cmd, char *argument )
 	   spell_full_heal( 60, ch, 0, 0, ch, 0);
       break;
    }
-}
-
-// This is a proc for the Leviathan mob.
-int leviathan( P_char ch, P_char pl, int cmd, char *arg )
-{
-   P_char tch;
-   int to_room;
-
-// ship_data * ship;
-
-   if( cmd == CMD_SET_PERIODIC )
-      return TRUE;
-
-   if( !ch )
-      return TRUE;
-
-// random chance to nearby ships:
-// if( ship = find_ship( ch ) && number( 0, 3) )
-// {
-//       roll/whale dive damaging ships like a ram attack
-//       return TRUE;
-// }
-
-   if( cmd == CMD_PERIODIC && !number( 0, 1 ) )
-   {
-      switch( number( 1, 2 ) )
-      {
-      case 1:
-         act( "$N lifts up out of the water, then splashes back down, causing a massive wave!", FALSE, ch, NULL, ch, TO_ROOM );
-
-         // To each char in room, chance of knockdown.
-         for( tch = world[ch->in_room].people;tch;tch = tch->next_in_room )
-         {
-            if( tch != ch )
-            {
-               if( number( 0, 1 ) )
-                  SET_POS( tch, POS_SITTING + GET_STAT(tch));
-               // if not knocked down, chance to get moved 1 room away.
-               else if( number( 0, 1 ) && (to_room = adjacent_room_nesw(ch, 1)) )
-               {
-                  // Move char 1 room
-                  char_from_room(tch);
-                  char_to_room(tch, to_room, -1);
-               }
-            }
-         }
-      break;
-      case 2:
-         tch = ch->specials.fighting;
-         if( tch )
-         {
-            act( "$N lashes out with a tentacle, wrapping it around you, lifts and quickly slams you upon the water surface!", FALSE, tch, NULL, ch, TO_CHAR );
-            act( "$N lashes out grabbing $n with a tentacle, thrashing $m into the water!", FALSE, tch, NULL, ch, TO_ROOM );
-            // Move victim 1-3 rooms away
-            if( to_room = adjacent_room_nesw(ch, number( 1, 3 )) )
-            {
-               char_from_room(tch);
-               char_to_room(tch, to_room, -1);
-            }
-            stop_fighting( tch );
-            // Stun for 3-5 sec
-            CharWait( tch, number( 3, 5 ) );
-         }
-      default:
-      break;
-      }
-   }
-
-   return FALSE;
 }
 
 // It's a percentage chance to make them attack a few extra times.
@@ -224,4 +157,115 @@ int adjacent_room_nesw(P_char ch, int num_rooms )
 
    // If no exit found, return -1;
    return -1;
+}
+
+// This is a proc for the Leviathan mob.
+int leviathan( P_char ch, P_char pl, int cmd, char *arg )
+{
+   P_char tch;
+   int to_room;
+   P_ship ship;
+   int ram_damage, heading;
+
+   if( cmd == CMD_SET_PERIODIC )
+      return TRUE;
+
+   if( !ch )
+      return TRUE;
+
+   // Attack nearby ships (within 3 rooms ):
+   if( cmd == CMD_PERIODIC && !number( 0, 1 ) )
+      if( ship = leviathan_find_ship( ch, ch->in_room, 3 ) )
+      {
+          act_to_all_in_ship( ship,"$N speeds into your ship!", ch );
+          act( "$N speeds into a ship!", FALSE, ch, NULL, ch, TO_ROOM );
+          ram_damage = number( 10, 15 );
+          heading = number( 0, 3 );
+          ch_damage_hull( ch, ship, ram_damage, heading, number( 0, 99 ) );
+          update_ship_status( ship );
+          return TRUE;
+      }
+
+   if( cmd == CMD_PERIODIC && !number( 0, 1 ) )
+   {
+      switch( number( 1, 2 ) )
+      {
+      case 1:
+         act( "$N lifts up out of the water, then splashes back down, causing a massive wave!", FALSE, ch, NULL, ch, TO_ROOM );
+
+         // To each char in room, chance of knockdown.
+         for( tch = world[ch->in_room].people;tch;tch = tch->next_in_room )
+         {
+            if( tch != ch )
+            {
+               if( number( 0, 1 ) )
+                  SET_POS( tch, POS_SITTING + GET_STAT(tch));
+               // if not knocked down, chance to get moved 1 room away.
+               else if( number( 0, 1 ) && (to_room = adjacent_room_nesw(ch, 1)) )
+               {
+                  // Move char 1 room
+                  char_from_room(tch);
+                  char_to_room(tch, to_room, -1);
+               }
+            }
+         }
+      break;
+      case 2:
+         tch = ch->specials.fighting;
+         if( tch )
+         {
+            act( "$N lashes out with a tentacle, wrapping it around you, lifts and quickly slams you upon the water surface!", FALSE, tch, NULL, ch, TO_CHAR );
+            act( "$N lashes out grabbing $n with a tentacle, thrashing $m into the water!", FALSE, tch, NULL, ch, TO_ROOM );
+            // Move victim 1-3 rooms away
+            if( to_room = adjacent_room_nesw(ch, number( 1, 3 )) )
+            {
+               char_from_room(tch);
+               char_to_room(tch, to_room, -1);
+            }
+            stop_fighting( tch );
+            // Stun for 3-5 sec
+            CharWait( tch, number( 3, 5 ) );
+         }
+      default:
+      break;
+      }
+   }
+
+   return FALSE;
+}
+
+// Returns a ship if it's near Leviathan
+P_ship leviathan_find_ship( P_char leviathan, int room, int num_rooms )
+{
+   int i;
+   P_obj obj;
+   P_ship ship;
+   room_direction_data *exit;
+   char msg[100];
+
+   if( !leviathan )
+      return NULL;
+
+   // Look through contents
+   for(obj = world[room].contents;obj;obj = obj->next_content )
+      // If found a ship && percent >= 50
+      if( obj && (GET_ITEM_TYPE(obj) == ITEM_SHIP) && (obj->value[6] == 1) && number( 0, 1) )
+         return shipObjHash.find(obj);
+
+   // This is a bit repetative, but that's ok, it's for a small number.
+   if( num_rooms > 0 )
+   {
+      // We only check exits N, S, E and W.
+      for(i = 0; i < 4; i++ )
+      {
+         exit = world[room].dir_option[i];
+         if( exit && exit->to_room )
+         {
+            ship = leviathan_find_ship( leviathan, exit->to_room, num_rooms - 1 );
+            if( ship )
+               return ship;
+         }
+      }
+   }
+   return NULL;
 }
