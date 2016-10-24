e2295d02da208eeef1dfc682c814abc90b3aa32a
Lohrr <lohrr@durismud.com>
2016-10-24 14:34:58 +0000

    Added comfort lines to the who list (there are goods/evils online).

--- src/actinf.c
+++ src/actinf.c
@@ -6608,6 +6608,10 @@ void do_who(P_char ch, char *argument, int cmd)
         sprintf(who_output + strlen(who_output),
           "&+cTotal in-game connections: %d.&N\n\n&+rRecord number of connections this boot: %d.&n\n",
           total_ingame_connections, max_descs);
+      if( curr_ingame_good > 0 )
+        sprintf(who_output + strlen(who_output), "There are &+Ygood(s)&N online.\n" );
+      if( curr_ingame_evil > 0 )
+        sprintf(who_output + strlen(who_output), "There are &+Revil(s)&N online.\n" );
     }
     send_to_char(who_output, ch, LOG_NONE);
     strcpy(who_output, "");
